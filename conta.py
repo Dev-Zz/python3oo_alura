b8d2acdb62b17136fa3c5738a7266629cd55b000
zz-dev <zz-dev@outlook.com>
2022-06-21 19:37:34 -0400

    Initial commit

---
+++ conta.py
@@ -0,0 +1,23 @@
+
+
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print(f'Construindo Objeto -> {self}')
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print(f'Saldo: {self.__saldo} do titular: {self.__titular}')
+
+    def depositar(self, valor):
+        self.__saldo += valor
+
+    def sacar(self, valor):
+        self.__saldo -= valor
+
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
