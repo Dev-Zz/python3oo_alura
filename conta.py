79a21fb5bea205745c67f679c2cfb5cafb20813c
Jose Eduardo <zz-dev@outlook.com>
2022-06-24 18:05:42 -0400

    update

--- conta.py
+++ conta.py
@@ -15,9 +15,37 @@ class Conta:
     def depositar(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def sacar(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print(f'O valor {valor} passou o limite!')
 
     def transferir(self, valor, destino):
         self.sacar(valor)
         destino.depositar(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite 
+    
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
+
